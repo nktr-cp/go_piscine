0b8d226232291ab070a07eaef8e42dcf2300586e
nktr-cp <nktrcp@gmail.com>
2024-06-21 15:58:16 +0900

    done go02-ex02

---
+++ go_02/ex02/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"ex02/vender/piscine"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(piscine.IterativePower(4, 3))
+}
---
+++ go_02/ex02/vender/piscine/iterativepower.go
@@ -0,0 +1,10 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	result := 1
+	for i := 0; i < power; i++ {
+		result *= nb
+	}
+
+	return result
+}
