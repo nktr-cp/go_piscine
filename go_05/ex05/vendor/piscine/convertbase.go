28e99bdc26d642c555b525474b636c136c49c80b
nktr-cp <nktrcp@gmail.com>
2024-06-24 19:25:51 +0900

    done go05-ex05

---
+++ go_05/ex05/vendor/piscine/convertbase.go
@@ -0,0 +1,30 @@
+package piscine
+
+func AtoiBase(s string, base string) int {
+	result := 0
+	base_length := len(base)
+	for _, c := range s {
+		index := 0
+		for i, b := range base {
+			if c == b {
+				index = i
+				break
+			}
+		}
+
+		result = result*base_length + index
+	}
+
+	return result
+}
+
+func ConvertBase(nbr, baseFrom, baseTo string) string {
+	nbr_i := AtoiBase(nbr, baseFrom)
+	base_len := len(baseTo)
+	result := ""
+	for nbr_i > 0 {
+		result = string(baseTo[nbr_i%base_len]) + result
+		nbr_i /= base_len
+	}
+	return result
+}
