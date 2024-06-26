38dc73f7838e0c5ab4e3bdf51309080b4855cfb8
nktr-cp <nktrcp@gmail.com>
2024-06-26 22:19:20 +0900

    modified go02

--- go_02/ex00/vendor/piscine/iterativefactorial.go
+++ go_02/ex00/vendor/piscine/iterativefactorial.go
@@ -1,12 +1,20 @@
 package piscine
 
 // Errors (non possible values or overflows) will return 0.
-func IterativeFactorial(n int) int {
-	if n < 0 || n > 12 {
+func IterativeFactorial(nb int) int {
+	if nb < 0 {
 		return 0
 	}
+
+	intSize := 32 << (^uint(0) >> 63)
+	if intSize == 32 && nb > 12 {
+		return 0
+	} else if intSize == 64 && nb > 20 {
+		return 0
+	}
+
 	result := 1
-	for i := 1; i <= n; i++ {
+	for i := 1; i <= nb; i++ {
 		result *= i
 	}
 	return result
--- go_02/ex02/vendor/piscine/iterativepower.go
+++ go_02/ex02/vendor/piscine/iterativepower.go
@@ -4,6 +4,12 @@ func IterativePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
+	intSize := 32 << (^uint(0) >> 63)
+	if intSize == 32 && nb > 12 {
+		return 0
+	} else if intSize == 64 && nb > 20 {
+		return 0
+	}
 	result := 1
 	for i := 0; i < power; i++ {
 		result *= nb
