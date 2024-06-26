1d52813582492c21410c1bd0346f7834d7362f25
nktr-cp <nktrcp@gmail.com>
2024-06-26 19:44:31 +0900

    modified go02

--- go_02/ex00/vendor/piscine/iterativefactorial.go
+++ go_02/ex00/vendor/piscine/iterativefactorial.go
@@ -1,10 +1,13 @@
 package piscine
 
+// Errors (non possible values or overflows) will return 0.
 func IterativeFactorial(n int) int {
+	if n < 0 || n > 12 {
+		return 0
+	}
 	result := 1
 	for i := 1; i <= n; i++ {
 		result *= i
 	}
-
 	return result
 }
--- go_02/ex01/vendor/piscine/recursivefactorial.go
+++ go_02/ex01/vendor/piscine/recursivefactorial.go
@@ -1,6 +1,9 @@
 package piscine
 
 func RecursiveFactorial(n int) int {
+	if n < 0 || n > 12 {
+		return 0
+	}
 	if n == 0 {
 		return 1
 	} else {
