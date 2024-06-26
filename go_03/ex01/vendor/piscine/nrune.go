c152167a4c812dc535250f7720165045136004bd
nktr-cp <nktrcp@gmail.com>
2024-06-26 20:28:55 +0900

    modified go03

--- go_03/ex01/vendor/piscine/nrune.go
+++ go_03/ex01/vendor/piscine/nrune.go
@@ -1,10 +1,14 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	n--
+	length := 0
+	for range s {
+		length++
+	}
 	runes := []rune(s)
+	n--
 
-	if n < 0 || len(runes) <= n {
+	if n < 0 || length <= n {
 		return 0
 	} else {
 		return runes[n]
