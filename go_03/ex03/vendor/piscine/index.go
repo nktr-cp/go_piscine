98d30ac3281396dafa2feeefaf9fc4aabcfdd5bb
nktr-cp <nktrcp@gmail.com>
2024-06-27 17:40:46 +0900

    modified go03

--- go_03/ex03/vendor/piscine/index.go
+++ go_03/ex03/vendor/piscine/index.go
@@ -5,7 +5,11 @@ func Index(s string, toFind string) int {
 	for range toFind {
 		target_length++
 	}
-	for i := 0; i+target_length <= len(s); i++ {
+	source_length := 0
+	for range s {
+		source_length++
+	}
+	for i := 0; i+target_length <= source_length; i++ {
 		if s[i:i+target_length] == toFind {
 			return i
 		}
