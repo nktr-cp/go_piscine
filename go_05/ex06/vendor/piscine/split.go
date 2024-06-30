eba047531009abd49c103155eaa6c5782fd9a31a
nktr-cp <nktrcp@gmail.com>
2024-06-30 22:12:15 +0900

    done go05

--- go_05/ex06/vendor/piscine/split.go
+++ go_05/ex06/vendor/piscine/split.go
@@ -3,4 +3,29 @@ package piscine
 func Split(s, sep string) []string {
 	var result []string
 
+	if sep == "" {
+		result = append(result, s)
+		return result
+	}
+
+	source_length := 0
+	for range s {
+		source_length++
+	}
+	sep_length := 0
+	for range sep {
+		sep_length++
+	}
+	start := 0
+	for i := 0; i < source_length; i++ {
+		if i == source_length-1 {
+			result = append(result, s[start:])
+			break
+		}
+		if s[i:i+sep_length] == sep {
+			result = append(result, s[start:i])
+			start = i + sep_length
+		}
+	}
+	return result
 }
