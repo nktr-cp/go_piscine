90a0f5c2af85e77cc17a32739a554d458cd4d7bc
nktr-cp <nktrcp@gmail.com>
2024-07-02 22:56:30 +0900

    done go07-ex04

---
+++ go_07/ex04/vendor/piscine/issorted.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	elem_count := 0
+	for range a {
+		elem_count++
+	}
+
+	for i := 0; i < elem_count-1; i++ {
+		if f(a[i], a[i+1]) == 1 {
+			return false
+		}
+	}
+	return true
+}
