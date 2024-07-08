d2f375aceefab3cab0d5ea6e46988eeb7f3fa617
nktr-cp <nktrcp@gmail.com>
2024-07-08 16:47:48 +0900

    done go09-ex10

---
+++ go_09/ex10/vendor/piscine/listremoveif.go
@@ -0,0 +1,38 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	toAdd := &NodeL{Data: data, Next: nil}
+	if l.Head == nil {
+		l.Head = toAdd
+		l.Tail = toAdd
+	} else {
+		l.Tail.Next = toAdd
+		l.Tail = toAdd
+	}
+}
+
+func ListRemoveIf(l *List, data_ref interface{}) {
+	for l.Head != nil && l.Head.Data == data_ref {
+		l.Head = l.Head.Next
+	}
+	if l.Head == nil {
+		return
+	}
+	prev := l.Head
+	for prev.Next != nil {
+		if prev.Next.Data == data_ref {
+			prev.Next = prev.Next.Next
+		} else {
+			prev = prev.Next
+		}
+	}
+}
