c67ec467cf950568c2e663f68801096655aee164
nktr-cp <nktrcp@gmail.com>
2024-07-08 14:28:38 +0900

    done go09-ex09

---
+++ go_09/ex09/vendor/piscine/listfind.go
@@ -0,0 +1,36 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	toAdd := &NodeL{Data: data, Next: nil}
+	if l.Head == nil {
+		l.Head = toAdd
+		l.Tail = toAdd
+	} else {
+		l.Tail.Next = toAdd
+		l.Tail = toAdd
+	}
+}
+
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
+
+func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
+	cur := l.Head
+	for cur != nil {
+		if comp(cur.Data, ref) {
+			return &cur.Data
+		}
+		cur = cur.Next
+	}
+	return nil
+}
