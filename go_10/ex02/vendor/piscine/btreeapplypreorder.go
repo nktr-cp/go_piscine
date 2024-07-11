4cd3346c7c3b8a81119f72eee3e2bd790a224721
nktr-cp <nktrcp@gmail.com>
2024-07-11 21:46:14 +0900

    modified go10-ex02

--- go_10/ex02/vendor/piscine/btreeapplypreorder.go
+++ go_10/ex02/vendor/piscine/btreeapplypreorder.go
@@ -25,11 +25,11 @@ func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	if root.Left != nil {
-		BTreeApplyPreorder(root.Left, f)
-	}
 	if _, err := f(root.Data); err != nil {
 		return
+	}
+	if root.Left != nil {
+		BTreeApplyPreorder(root.Left, f)
 	}
 	if root.Right != nil {
 		BTreeApplyPreorder(root.Right, f)
