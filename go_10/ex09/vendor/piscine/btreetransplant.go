74f5825274a3a5a8daff7d17d7519da1d1aac03e
nktr-cp <nktrcp@gmail.com>
2024-07-11 21:55:37 +0900

    modified go10-ex09

--- go_10/ex09/vendor/piscine/btreetransplant.go
+++ go_10/ex09/vendor/piscine/btreetransplant.go
@@ -49,9 +49,13 @@ func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 }
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
+	if root == nil || node == nil {
+		return root
+	}
 	if node.Parent == nil {
 		root = rplc
-	} else if node == node.Parent.Left {
+	}
+	if node == node.Parent.Left {
 		node.Parent.Left = rplc
 	} else {
 		node.Parent.Right = rplc
